7432577f6be2f375f542976075ecbc5ed7c3c4dc
Warman Suganda <warmansuganda@yahoo.com>
2012-07-07 23:58:45 +0700

    Commit Router.php

---
+++ Router.php
@@ -0,0 +1,24 @@
+<?php
+
+Web::$host = $_SERVER['SERVER_NAME']; //'ecp.net';
+//echo md5($_SERVER['SERVER_NAME']);
+switch (Web::getSubDomain()) {
+    case 'www':
+        Web::main('SMPN 1 SUBANG', 'main', 'elegant');
+        Web::child('ADMIN SMPN 1 SUBANG', 'admin_main', 'demo');
+        break;
+    case 'digilib':
+        Web::main('DIGITAL LIBRARY', 'digilib', 'elegant');
+        Web::child('ADMIN DIGITAL LIBRARY', 'admin', 'digilib_master', 'demo');
+        Web::child('OPERATOR DIGITAL LIBRARY', 'operator', 'digilib_master', 'demo');
+        break;
+    case 'demo':
+        Web::main('DIGITAL LIBRARY', 'demo', 'demo');
+        Web::child('ADMIN DIGITAL LIBRARY', 'admin_digilib', 'demo');
+        break;
+    default:
+        Web::main('DEMO', 'demo', 'demo');
+        Web::child('Admin E-Commerce Portal', 'demo', 'demo');
+        break;
+}
+
