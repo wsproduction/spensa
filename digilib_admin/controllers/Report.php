82e24503799cb182afbbae3a13bdb19b8c71f0c8
kit <warmansuganda@yahoo.com>
2013-01-19 12:17:08 +0700

    progress

--- digilib_admin/controllers/Report.php
+++ digilib_admin/controllers/Report.php
@@ -12,8 +12,17 @@ class Report extends Controller {
     }
 
     public function borrow() {
-        Web::setTitle('Laporan Peminjaman Buku');
-        $this->view->render('report/index');
+        Web::setTitle('Laporan Buku Yang Dipinjam');
+        $this->view->month_option = $this->content->monthList();
+        $this->view->year_option = $this->content->yearList();
+        $this->view->render('report/borrow');
+    }
+
+    public function borrower() {
+        Web::setTitle('Laporan Peminjam Buku');
+        $this->view->month_option = $this->content->monthList();
+        $this->view->year_option = $this->content->yearList();
+        $this->view->render('report/borrower');
     }
 
     public function pinalty() {
@@ -41,7 +50,7 @@ class Report extends Controller {
             $pdf->SetSubject('Koleksi Buku');
 
             // set default header data
-            $pdf->SetHeaderData('', '', 'PERPUSTAKAAN', 'SMP NEGERI 1 SUBANG');
+            $pdf->setPrintHeader(false);
 
             // set header and footer fonts
             $pdf->setHeaderFont(Array(PDF_FONT_NAME_MAIN, '', PDF_FONT_SIZE_MAIN));
@@ -51,7 +60,7 @@ class Report extends Controller {
             $pdf->SetDefaultMonospacedFont(PDF_FONT_MONOSPACED);
 
             //set margins
-            $pdf->SetMargins(11, 20, 11);
+            $pdf->SetMargins(11, 10, 11);
             $pdf->SetHeaderMargin(PDF_MARGIN_HEADER);
             $pdf->SetFooterMargin(PDF_MARGIN_FOOTER);
 
@@ -80,8 +89,14 @@ class Report extends Controller {
                                 border-right : 1px solid #000;
                                 border-bottom : 1px solid #000;
                             }
+                            table.list .valign-middle {
+                                line-height:7px;
+                            }
                             .first {
                                 border-left : 1px solid #000;
+                            }
+                            .no-border_right {
+                                border-left : none;
                             }
                         </style>';
             $html .= '<div style="text-align:center;font-weight:bold">';
@@ -91,25 +106,42 @@ class Report extends Controller {
 
             $html .= '<div>';
             $html .= '<table class="list" width="100%">';
+            $html .= '  <thead>';
             $html .= '  <tr>';
-            $html .= '      <th width="60" align="center" class="first">NO.</th>';
-            $html .= '      <th width="280" >&nbsp;NAMA ANGGOTA</th>';
-            $html .= '      <th width="165" align="center">JUMLAH SANKSI</th>';
-            $html .= '      <th width="165" align="center">TOTAL DENDA</th>';
+            $html .= '      <th width="40" height="35" align="center" valign="middle" class="first valign-middle">NO.</th>';
+            $html .= '      <th width="75" class="valign-middle" align="center">ID</th>';
+            $html .= '      <th width="180" class="valign-middle" >&nbsp;NAMA ANGGOTA</th>';
+            $html .= '      <th width="100" class="valign-middle" align="center">JABATAN</th>';
+            $html .= '      <th width="100" class="valign-middle" align="center">KETERANGAN</th>';
+            $html .= '      <th width="70" align="center" style="line-height:4px;">JUMLAH SANKSI</th>';
+            $html .= '      <th width="100" class="valign-middle" align="center">TOTAL DENDA</th>';
             $html .= '  </tr>';
-
+            $html .= '  </thead>';
+            
+            $idx = 1;
+            $pinalty_count= 0;
+            $pinalty_total = 0;
             foreach ($data as $value) {
                 $html .= '  <tr>';
-                $html .= '      <td width="60" align="center" class="first">NO.</td>';
-                $html .= '      <td width="280" >&nbsp;NAMA ANGGOTA</td>';
-                $html .= '      <td width="165" align="center">JUMLAH SANKSI</td>';
-                $html .= '      <td width="165" align="center">TOTAL DENDA</td>';
+                $html .= '      <td width="40" align="center" class="first">' . $idx . '</td>';
+                $html .= '      <td width="75" align="center">&nbsp;' . $value['members_id'] . '</td>';
+                $html .= '      <td width="180" >&nbsp;' . $value['members_name'] . '</td>';
+                $html .= '      <td width="100">&nbsp;' . $value['isa_title'] . '</td>';
+                $html .= '      <td width="100">&nbsp;' . $value['members_desc'] . '</td>';
+                $html .= '      <td width="70" align="center">' . $value['pinalty_count'] . '</td>';
+                $html .= '      <td width="20"align="center" style="border-right: none;">Rp.</td>';
+                $html .= '      <td width="80"align="right">' . $value['pinalty_total'] . '</td>';
                 $html .= '  </tr>';
+                $idx++;
+                $pinalty_count += $value['pinalty_count'];
+                $pinalty_total += $value['pinalty_total'];
             }
 
             $html .= '  <tr>';
-            $html .= '      <td colspan="3" align="center" class="first"><b>TOTAL</b></td>';
-            $html .= '      <td width="165" align="center"></td>';
+            $html .= '      <td colspan="5" align="center" class="first"><b>TOTAL</b></td>';
+            $html .= '      <td align="center">' . $pinalty_count . '</td>';
+            $html .= '      <td width="20" align="center" style="border-right: none;">Rp.</td>';
+            $html .= '      <td align="right">' . $pinalty_total . '</td>';
             $html .= '  </tr>';
 
             $html .= '</table>';
@@ -120,7 +152,235 @@ class Report extends Controller {
             //Close and output PDF document
             $pdf->Output('pinalty_' . date('dmYHis') . '.pdf', 'I');
         } else {
-            echo 'Maaf Daftar Print Tidak Ditemukan!';
+            echo 'Maaf Data Pada Periode Ini Tidak Ditemukan';
+        }
+    }
+
+    public function readBorrower($period) {
+        $period = str_pad($period, 6, "0", STR_PAD_LEFT);
+        $data = $this->model->selectBorrowerByPeriod($period);
+
+        $month = substr($period, 0, 2);
+        $year = substr($period, -4);
+
+        if (count($data) > 0) {
+
+            $pdf = Src::plugin()->tcPdf();
+
+            // set document information
+            $pdf->SetCreator(PDF_CREATOR);
+            $pdf->SetAuthor('Warman Suganda');
+            $pdf->SetTitle('Cetak Barcode Buku Induk');
+            $pdf->SetSubject('Koleksi Buku');
+
+            // set default header data
+            $pdf->setPrintHeader(false);
+
+            // set header and footer fonts
+            $pdf->setHeaderFont(Array(PDF_FONT_NAME_MAIN, '', PDF_FONT_SIZE_MAIN));
+            $pdf->setFooterFont(Array(PDF_FONT_NAME_DATA, '', PDF_FONT_SIZE_DATA));
+
+            // set default monospaced font
+            $pdf->SetDefaultMonospacedFont(PDF_FONT_MONOSPACED);
+
+            //set margins
+            $pdf->SetMargins(11, 10, 11);
+            $pdf->SetHeaderMargin(PDF_MARGIN_HEADER);
+            $pdf->SetFooterMargin(PDF_MARGIN_FOOTER);
+
+            //set auto page breaks
+            $pdf->SetAutoPageBreak(FALSE, PDF_MARGIN_BOTTOM);
+
+            //set image scale factor
+            $pdf->setImageScale(PDF_IMAGE_SCALE_RATIO);
+
+            // set font
+            $pdf->SetFont('helvetica', '', 11);
+
+            // add a page
+            $pdf->AddPage();
+            $pdf->SetFont('helvetica', '', 10);
+
+            $html = '  <style>
+                            table.list tr th {
+                                border-right : 1px solid #000;
+                                border-top : 1px solid #000;
+                                border-bottom : 1px solid #000;
+                                font-weight : bold;
+                                background-color : #ccc;
+                            }
+                            table.list tr td {
+                                border-right : 1px solid #000;
+                                border-bottom : 1px solid #000;
+                            }
+                            table.list .valign-middle {
+                                line-height:7px;
+                            }
+                            .first {
+                                border-left : 1px solid #000;
+                            }
+                            .no-border_right {
+                                border-left : none;
+                            }
+                        </style>';
+            $html .= '<div style="text-align:center;font-weight:bold">';
+            $html .= 'LAPORAN PEMINJAM BUKU<br>';
+            $html .= 'PERIODE BULAN ' . strtoupper($this->content->monthName($month)) . ' ' . $year;
+            $html .= '</div>';
+
+            $html .= '<div>';
+            $html .= '<table class="list" width="100%">';
+            $html .= '  <thead>';
+            $html .= '  <tr>';
+            $html .= '      <th width="40" height="35" align="center" valign="middle" class="first valign-middle">NO.</th>';
+            $html .= '      <th width="90" class="valign-middle" align="center">ID</th>';
+            $html .= '      <th width="200" class="valign-middle" >&nbsp;NAMA ANGGOTA</th>';
+            $html .= '      <th width="100" class="valign-middle" align="center">JABATAN</th>';
+            $html .= '      <th width="100" class="valign-middle" align="center">KETERANGAN</th>';
+            $html .= '      <th width="140" align="center" class="valign-middle">JUMLAH PINJAMAN</th>';
+            $html .= '  </tr>';
+            $html .= '  </thead>';
+            
+            $idx = 1;
+            $borrow_count = 0;
+            foreach ($data as $value) {
+                $html .= '  <tr>';
+                $html .= '      <td width="40" align="center" class="first">' . $idx . '</td>';
+                $html .= '      <td width="90" align="center">&nbsp;' . $value['members_id'] . '</td>';
+                $html .= '      <td width="200" >&nbsp;' . $value['members_name'] . '</td>';
+                $html .= '      <td width="100">&nbsp;' . $value['isa_title'] . '</td>';
+                $html .= '      <td width="100">&nbsp;' . $value['members_desc'] . '</td>';
+                $html .= '      <td width="140" align="center">' . $value['borrow_count'] . '</td>';
+                $html .= '  </tr>';
+                $idx++;
+                $borrow_count += $value['borrow_count'];
+            }
+
+            $html .= '  <tr>';
+            $html .= '      <td colspan="5" align="center" class="first"><b>TOTAL</b></td>';
+            $html .= '      <td align="center">' . $borrow_count . '</td>';
+            $html .= '  </tr>';
+
+            $html .= '</table>';
+            $html .= '</div>';
+
+            $pdf->writeHTML($html);
+
+            //Close and output PDF document
+            $pdf->Output('pinalty_' . date('dmYHis') . '.pdf', 'I');
+        } else {
+            echo 'Maaf Data Pada Periode Ini Tidak Ditemukan';
+        }
+    }
+
+    public function readBorrow($period) {
+        $period = str_pad($period, 6, "0", STR_PAD_LEFT);
+        $data = $this->model->selectBorrowByPeriod($period);
+
+        $month = substr($period, 0, 2);
+        $year = substr($period, -4);
+
+        if (count($data) > 0) {
+
+            $pdf = Src::plugin()->tcPdf();
+
+            // set document information
+            $pdf->SetCreator(PDF_CREATOR);
+            $pdf->SetAuthor('Warman Suganda');
+            $pdf->SetTitle('Cetak Barcode Buku Induk');
+            $pdf->SetSubject('Koleksi Buku');
+
+            // set default header data
+            $pdf->setPrintHeader(false);
+
+            // set header and footer fonts
+            $pdf->setHeaderFont(Array(PDF_FONT_NAME_MAIN, '', PDF_FONT_SIZE_MAIN));
+            $pdf->setFooterFont(Array(PDF_FONT_NAME_DATA, '', PDF_FONT_SIZE_DATA));
+
+            // set default monospaced font
+            $pdf->SetDefaultMonospacedFont(PDF_FONT_MONOSPACED);
+
+            //set margins
+            $pdf->SetMargins(11, 10, 11);
+            $pdf->SetHeaderMargin(PDF_MARGIN_HEADER);
+            $pdf->SetFooterMargin(PDF_MARGIN_FOOTER);
+
+            //set auto page breaks
+            $pdf->SetAutoPageBreak(FALSE, PDF_MARGIN_BOTTOM);
+
+            //set image scale factor
+            $pdf->setImageScale(PDF_IMAGE_SCALE_RATIO);
+
+            // set font
+            $pdf->SetFont('helvetica', '', 11);
+
+            // add a page
+            $pdf->AddPage();
+            $pdf->SetFont('helvetica', '', 10);
+
+            $html = '  <style>
+                            table.list tr th {
+                                border-right : 1px solid #000;
+                                border-top : 1px solid #000;
+                                border-bottom : 1px solid #000;
+                                font-weight : bold;
+                                background-color : #ccc;
+                            }
+                            table.list tr td {
+                                border-right : 1px solid #000;
+                                border-bottom : 1px solid #000;
+                            }
+                            table.list .valign-middle {
+                                line-height:7px;
+                            }
+                            .first {
+                                border-left : 1px solid #000;
+                            }
+                            .no-border_right {
+                                border-left : none;
+                            }
+                        </style>';
+            $html .= '<div style="text-align:center;font-weight:bold">';
+            $html .= 'LAPORAN BUKU YANG DIPINJAM<br>';
+            $html .= 'PERIODE BULAN ' . strtoupper($this->content->monthName($month)) . ' ' . $year;
+            $html .= '</div>';
+
+            $html .= '<div>';
+            $html .= '<table class="list" width="100%">';
+            $html .= '  <thead>';
+            $html .= '  <tr>';
+            $html .= '      <th width="40" height="35" align="center" valign="middle" class="first valign-middle">NO.</th>';
+            $html .= '      <th width="90" class="valign-middle" align="center">ID BUKU</th>';
+            $html .= '      <th width="400" class="valign-middle" >&nbsp;JUDUL BUKU</th>';
+            $html .= '      <th width="140" align="center" class="valign-middle">JUMLAH PINJAMAN</th>';
+            $html .= '  </tr>';
+            $html .= '  </thead>';
+            
+            $idx = 1;
+            foreach ($data as $value) {
+                $html .= '  <tr>';
+                $html .= '      <td width="40" align="center" class="first">' . $idx . '</td>';
+                $html .= '      <td width="90" align="center">&nbsp;' . $value['book_id'] . '</td>';
+                $html .= '      <td width="400" >&nbsp;' . $value['book_title'] . '</td>';
+                $html .= '      <td width="140" align="center">' . $value['book_publishing'] . '</td>';
+                $html .= '  </tr>';
+                $idx++;
+            }
+
+            $html .= '  <tr>';
+            $html .= '      <td colspan="3" align="center" class="first"><b>TOTAL</b></td>';
+            $html .= '      <td align="center"></td>';
+            $html .= '  </tr>';
+
+            $html .= '</table>';
+            $html .= '</div>';
+
+            $pdf->writeHTML($html);
+
+            //Close and output PDF document
+            $pdf->Output('pinalty_' . date('dmYHis') . '.pdf', 'I');
+        } else {
+            echo 'Maaf Data Pada Periode Ini Tidak Ditemukan';
         }
     }
 
